ff90fecd7e467b0e3918d924f27a266ede5c74e9
Paradigma-corp <dvdlf@pm.me>
2025-07-08 20:26:33 -0500

    Actualización ruta

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Prefer loading of ES Modules over CommonJS
-  experimental: { esmExternals: true },
-  reactStrictMode: false,
-};
-
-export default nextConfig;
