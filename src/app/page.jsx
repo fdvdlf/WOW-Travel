f4dac0382478fa0b22b7e61a24d9da0a5fb69447
Paradigma-corp <dvdlf@pm.me>
2025-11-06 11:39:10 -0500

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import { AboutOne, AboutTwo } from "@/components/about/AboutTwo";
 import { BannerOne, BannerTwo } from "@/components/banner/BannerTwo";
 import { BlogOne } from "@/components/blogs/BlogOne";
-import { brandTwo } from "@/components/brands/brandTwo";
+import { BrandTwo } from "@/components/brands/brandTworandTwo";
 import { CounterOne } from "@/components/counter/CounterOne";
 import { InstagramOne } from "@/components/insta/InstagramOne";
 import { MarqueeOne } from "@/components/marquee/MarqueeOne";
@@ -34,7 +34,7 @@ export default function Home() {
       <CounterOne />
 
       {/* brand */}
-      <BrandOne />
+      <BrandTwo />
 
       {/* team */}
       <TeamOne />
