0e35bcd378bfde52d075b22f938732e4c3252ac1
Paradigma-corp <dvdlf@pm.me>
2025-11-17 20:49:29 -0500

    Center header logo with subtle animation

--- src/components/header/HeaderThree.jsx
+++ src/components/header/HeaderThree.jsx
@@ -19,26 +19,20 @@ export const HeaderThree = () => {
 
         <div
           id="sticky-header"
-          className="tg-header__area tg-header__area-three"
+          className="tg-header__area tg-header__area-three header-float"
           style={{ backgroundColor: "#ffffff" }}
         >
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
-                <div className="tgmenu__wrap">
-                  <div className="row align-items-center justify-content-center">
-                    <div className="col-xl-5 d-none d-xl-block">{/* Menú eliminado */}</div>
-
-                    <div className="col-xl-2 col-md-6 col-10">
-                      <div className="logo text-center text-md-center">
-                        <Link href="/">
-                          <img src={wLogo} alt="Logo" />
-                        </Link>
-                      </div>
-                    </div>
+                <div className="tgmenu__wrap tgmenu__wrap-centered">
+                  <div className="logo text-center">
+                    <Link href="/">
+                      <img src={wLogo} alt="Logo" />
+                    </Link>
                   </div>
 
-                  <div className="mobile-nav-toggler">
+                  <div className="mobile-nav-toggler" aria-label="Abrir menú">
                     <i className="flaticon-layout"></i>
                   </div>
                 </div>
